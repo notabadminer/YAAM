2fff2e8d7b6d75e14ca565e887a7b0540a4cdb46
Steve <mikomuto@gmail.com>
2016-03-29 13:47:07 -0700

    A couple changes
    -added mod config options to forge start menu gui
    -fixed issue with handheld item rotation

--- src/main/java/yaam/YAAM.java
+++ src/main/java/yaam/YAAM.java
@@ -8,48 +8,43 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import yaam.proxy.CommonProxy;
 
-@Mod(modid = YAAM.MODID, 
-	name = YAAM.NAME, 
-	version = YAAM.VERSION, 
-	acceptedMinecraftVersions = "@MC_VERSION@", 
-	updateJSON = "https://raw.githubusercontent.com/notabadminer/YAAM/master/version.json")
+@Mod(modid = YAAM.MODID, name = YAAM.NAME, version = YAAM.VERSION, acceptedMinecraftVersions = "@MC_VERSION@", guiFactory = "yaam.YAAMGuiFactory", updateJSON = "https://raw.githubusercontent.com/notabadminer/YAAM/master/version.json")
 
 public class YAAM {
 	public static final String MODID = "yaam";
 	public static final String NAME = "YAAM";
 	public static final String VERSION = "@VERSION@";
 	public static boolean obsidianEnabled, quartzEnabled, emeraldEnabled, lapisEnabled, nativePaxelsEnabled;
+	public static Configuration config;
 
 	@SidedProxy(clientSide = "yaam.proxy.ClientProxy", serverSide = "yaam.proxy.CommonProxy")
 	public static CommonProxy proxy;
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-		config.load();
+		config = new Configuration(event.getSuggestedConfigurationFile());
 
 		// custom items
-		Property obsidianRecipes = config.get("Recipes", "Obsidain Item Recipes", true);
+		Property obsidianRecipes = config.get("recipes", "Obsidain Item Recipes", true);
 		obsidianRecipes.setComment("Set to false to disable crafting recipes for obsidain tools, weapons, and armor.");
 		obsidianEnabled = obsidianRecipes.getBoolean(true);
-		Property emeraldRecipes = config.get("Recipes", "Emerald Item Recipes", true);
+		Property emeraldRecipes = config.get("recipes", "Emerald Item Recipes", true);
 		emeraldRecipes.setComment("Set to false to disable crafting recipes for emerald tools, weapons, and armor.");
 		emeraldEnabled = emeraldRecipes.getBoolean(true);
-		Property quartzRecipes = config.get("Recipes", "Quartz Item Recipes", true);
+		Property quartzRecipes = config.get("recipes", "Quartz Item Recipes", true);
 		quartzRecipes.setComment("Set to false to disable crafting recipes for quartz tools, weapons, and armor.");
 		quartzEnabled = quartzRecipes.getBoolean(true);
-		Property lapisRecipes = config.get("Recipes", "Lapis Item Recipes", true);
-		quartzRecipes.setComment("Set to false to disable crafting recipes for lapis tools, weapons, and armor.");
+		Property lapisRecipes = config.get("recipes", "Lapis Item Recipes", true);
+		lapisRecipes.setComment("Set to false to disable crafting recipes for lapis tools, weapons, and armor.");
 		lapisEnabled = lapisRecipes.getBoolean(true);
 
 		// native paxels
-		Property nativePaxels = config.get("Recipes", "Native Paxel Recipes", true);
+		Property nativePaxels = config.get("recipes", "Native Paxel Recipes", true);
 		nativePaxels
 				.setComment("Set to false to disable crafting recipes for wood, stone, iron, gold, and diamond paxels");
 		nativePaxelsEnabled = nativePaxels.getBoolean(true);
 
 		config.save();
-
 	}
 
 	@Mod.EventHandler
