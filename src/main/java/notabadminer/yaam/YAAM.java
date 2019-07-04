3f3448cbcc546cc0b9c5326c413419a606840b71
Steve <notabadminer@users.noreply.github.com>
2019-07-03 19:36:09 -0700

    update
    *added item tiers (tool materials)
    *started work on paxels

--- src/main/java/notabadminer/yaam/YAAM.java
+++ src/main/java/notabadminer/yaam/YAAM.java
@@ -2,18 +2,18 @@ package notabadminer.yaam;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemTier;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
+import notabadminer.yaam.items.YAAMPaxelItem;
 import notabadminer.yaam.items.YAAMSwordItem;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("yaam")
 public class YAAM {
-	public static Item quartzSword, obsidianSword;
+	public static Item quartz_sword, obsidian_sword, wood_paxel;
 
     public YAAM() {
         // Register ourselves for server and other game events we are interested in
@@ -27,10 +27,13 @@ public class YAAM {
         @SubscribeEvent
         public static void onItemRegistry(final RegistryEvent.Register<Item> itemRegistryEvent) {
             // register a new item here
-            quartzSword = new YAAMSwordItem(ItemTier.DIAMOND, 3, -2.4F, (new Item.Properties()).group(ItemGroup.COMBAT)).setRegistryName("quartz_sword");
-            obsidianSword = new YAAMSwordItem(ItemTier.DIAMOND, 3, -2.4F, (new Item.Properties()).group(ItemGroup.COMBAT)).setRegistryName("obsidian_sword");
-        	ForgeRegistries.ITEMS.register(quartzSword);
-        	ForgeRegistries.ITEMS.register(obsidianSword);
+            quartz_sword = new YAAMSwordItem(YAAMItemTier.QUARTZ, 3, -2.4F, (new Item.Properties()).group(ItemGroup.COMBAT)).setRegistryName("quartz_sword");
+            obsidian_sword = new YAAMSwordItem(YAAMItemTier.OBSIDIAN, 3, -2.4F, (new Item.Properties()).group(ItemGroup.COMBAT)).setRegistryName("obsidian_sword");
+            wood_paxel = new YAAMPaxelItem(YAAMItemTier.PAXELWOOD, 6.0F, -3.2F, (new Item.Properties()).group(ItemGroup.TOOLS)).setRegistryName("wood_paxel");
+
+        	ForgeRegistries.ITEMS.register(quartz_sword);
+        	ForgeRegistries.ITEMS.register(obsidian_sword);
+        	ForgeRegistries.ITEMS.register(wood_paxel);
         }
     }
 
---
+++ src/main/java/notabadminer/yaam/items/YAAMPaxelItem.java
@@ -0,0 +1,12 @@
+package notabadminer.yaam.items;
+
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.IItemTier;
+
+public class YAAMPaxelItem extends AxeItem {
+
+	public YAAMPaxelItem(IItemTier tier, float p_i48530_2_, float attackSpeedIn, Properties builder) {
+		super(tier, p_i48530_2_, attackSpeedIn, builder);
+		// TODO Auto-generated constructor stub
+	}
+}
